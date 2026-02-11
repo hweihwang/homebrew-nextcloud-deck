d832a0a9c983683a1b86d663d10d58a2c4732f7e
Hoang Pham <hoangmaths96@gmail.com>
2026-02-11 19:06:35 +0700

    chore(homebrew): update nextcloud-deck cask v0.1.0

---
+++ Casks/nextcloud-deck.rb
@@ -0,0 +1,25 @@
+cask "nextcloud-deck" do
+  arch arm: "arm64", intel: "x64"
+
+  version "0.1.0"
+  sha256 arm: "238ea0048fd31c0070fefe13637e34d836d1880f0c86efb0943af4c59eaf55eb", intel: "5240eeae831a638582024310b77fe0a64d2f7caa5e0fa47371ee9fef14c95e44"
+
+  url "https://github.com/hweihwang/nextcloud-deck-desktop-releases/releases/download/v#{version}/NextcloudDeck-mac-#{arch}.dmg"
+  name "Nextcloud Deck"
+  desc "Nextcloud Deck desktop client for macOS."
+  homepage "https://deck.hweihwang.dev/"
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "Nextcloud Deck.app"
+
+  uninstall quit: "com.hweihwang.nextcloud-deck"
+
+  zap trash: [
+    "~/Library/Application Support/Nextcloud Deck",
+    "~/Library/Caches/com.hweihwang.nextcloud-deck",
+    "~/Library/Preferences/com.hweihwang.nextcloud-deck.plist",
+    "~/Library/Saved Application State/com.hweihwang.nextcloud-deck.savedState",
+  ]
+end
