f550850b6785bb3111cb2bc810025108dfac4036
Hoang Pham <hoangmaths96@gmail.com>
2026-02-11 19:13:40 +0700

    chore(homebrew): update nextcloud-deck cask v0.1.0

--- Casks/nextcloud-deck.rb
+++ Casks/nextcloud-deck.rb
@@ -5,6 +5,7 @@ cask "nextcloud-deck" do
   sha256 arm: "238ea0048fd31c0070fefe13637e34d836d1880f0c86efb0943af4c59eaf55eb", intel: "5240eeae831a638582024310b77fe0a64d2f7caa5e0fa47371ee9fef14c95e44"
 
   url "https://github.com/hweihwang/nextcloud-deck-desktop-releases/releases/download/v#{version}/NextcloudDeck-mac-#{arch}.dmg"
+  verified: "github.com/hweihwang/nextcloud-deck-desktop-releases/"
   name "Nextcloud Deck"
   desc "Nextcloud Deck desktop client for macOS."
   homepage "https://deck.hweihwang.dev/"
