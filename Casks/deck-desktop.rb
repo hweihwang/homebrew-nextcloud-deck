cd9b2b7e8e2966f2d9098ed8cd2a546f0ff2f4ea
Hoang Pham <hoangmaths96@gmail.com>
2026-02-22 15:42:37 +0700

    chore(homebrew): update deck-desktop cask v0.1.3

--- Casks/deck-desktop.rb
+++ Casks/deck-desktop.rb
@@ -1,10 +1,10 @@
 cask "deck-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.2"
-  sha256 arm: "f1376cc71a1b43b8099ca94ab8e06cf4ee53c08d734bf04a6a8378ee4c19032f", intel: "dc04d8c5ca9899186571487f09ef19ba129a67774ae39188ea11e40b57f952e1"
+  version "0.1.3"
+  sha256 arm: "34888db9c4c09b3a6e6a00e8a4a3ce6e3545f7ed235198d99b470a570266a7e0", intel: "1a4033ad036ce0303f01738d9a227003387df20476204288b064bc76c7c13bf9"
 
-  url "https://github.com/hweihwang/nextcloud-deck-desktop-releases/releases/download/v#{version}/deck-desktop-mac-#{arch}.dmg",
+  url "https://github.com/hweihwang/nextcloud-deck-desktop-releases/releases/download/v#{version}/deck-desktop-mac-#{arch}.zip",
       verified: "github.com/hweihwang/nextcloud-deck-desktop-releases/"
   name "Deck Desktop"
   desc "Unofficial desktop client for Nextcloud Deck on macOS."
