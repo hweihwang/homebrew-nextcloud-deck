212040864f80281b5eebe9c390c2e9324045aab3
Hoang Pham <hoangmaths96@gmail.com>
2026-02-27 18:45:56 +0700

    chore(homebrew): update deck-desktop cask v0.1.6

--- Casks/deck-desktop.rb
+++ Casks/deck-desktop.rb
@@ -1,13 +1,13 @@
 cask "deck-desktop" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.5"
-  sha256 arm: "f7b59be25bb5b0d3c580a7d547f92af74df727c351a8aca89351f3c355ec4590", intel: "3dc30077e9be30fbc85a77d90e12ec1d8fa2219df2850919e018e3773d40afa5"
+  version "0.1.6"
+  sha256 arm: "ee3e2a8ab521b36863ead4571f2a5cbdbd3d2bb294c666c39585c35555768ba6", intel: "205a24b4a9500acc0d95b606a74947d1d94d2fdf8088596b99fb7fa5ea28e8cb"
 
   url "https://github.com/hweihwang/nextcloud-deck-desktop-releases/releases/download/v#{version}/deck-desktop-mac-#{arch}.dmg",
       verified: "github.com/hweihwang/nextcloud-deck-desktop-releases/"
   name "Deckloud"
-  desc "Deckloud is an unofficial desktop client for Nextcloud Deck on macOS."
+  desc "Deckloud for macOS — a native Nextcloud Deck client. Also available on the App Store for iOS."
   homepage "https://deckloud.com"
 
   auto_updates true
