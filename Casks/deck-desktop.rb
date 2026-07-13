cc233a8549257d8d306526f4da038e019c54753a
Hoang Pham <hoangmaths96@gmail.com>
2026-07-14 00:58:55 +0700

    chore(homebrew): update deck-desktop cask v0.7.3

--- Casks/deck-desktop.rb
+++ Casks/deck-desktop.rb
@@ -1,6 +1,6 @@
 cask "deck-desktop" do
-  version "0.7.2"
-  sha256 "a1d617f0c84f94e563b44f45aa3eac7e9c2252913fe9ccba452f670843423f51"
+  version "0.7.3"
+  sha256 "1896449ddd9f1c61246d9c89771f04dd8a0b8acb3d02b868ec266b082843bd11"
 
   url "https://github.com/hweihwang/nextcloud-deck-desktop-releases/releases/download/v#{version}/stable-macos-arm64-Deckloud.dmg",
       verified: "github.com/hweihwang/nextcloud-deck-desktop-releases/"
